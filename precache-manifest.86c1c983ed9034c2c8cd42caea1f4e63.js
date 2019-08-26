966da8b04d9f8ef9a4168cf2e13cb11597b13906
Omulosi <mulongojohnpaul@gmail.com>
2019-08-27 00:56:36 +0300

    Updates

---
+++ precache-manifest.86c1c983ed9034c2c8cd42caea1f4e63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5acce7f78fc66ba6c4aec1d3588e7ad",
+    "url": "/library-project-react/index.html"
+  },
+  {
+    "revision": "cc9e2db3eaeb5a9f5f25",
+    "url": "/library-project-react/static/css/main.8944dcd7.chunk.css"
+  },
+  {
+    "revision": "d030b6347289affa0e46",
+    "url": "/library-project-react/static/js/2.e5aeddc0.chunk.js"
+  },
+  {
+    "revision": "cc9e2db3eaeb5a9f5f25",
+    "url": "/library-project-react/static/js/main.73eda4a6.chunk.js"
+  },
+  {
+    "revision": "40c0bad83b261f92d4fa",
+    "url": "/library-project-react/static/js/runtime~main.5c2b754a.js"
+  }
+]);
